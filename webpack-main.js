099c0f4318a16fe907551df2bc4a3c386e5e36e4
comoco <como65416@gmail.com>
2019-08-25 11:14:40 +0800

    add webpack config

---
+++ webpack-main.js
@@ -0,0 +1,5 @@
+import App from './src/App.js';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+render(<App/>, window.document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  target: 'electron-renderer',
+  entry: [
+    './webpack-main.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=50000'
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `bundle.css`,
+    })
+  ]
+};
